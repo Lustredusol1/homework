9b214adf96360162746d766737daf1acd57818dc
Lustredusol1 <katyasaraeva95@mail.ru>
2023-09-14 16:47:08 +0300

    final3

--- src/main/java/Ekaterina/homework/Controller/TaskController.java
+++ src/main/java/Ekaterina/homework/Controller/TaskController.java
@@ -2,7 +2,11 @@ package Ekaterina.homework.Controller;
 
 import Ekaterina.homework.Entity.Task;
 import Ekaterina.homework.Service.TaskService;
+import org.antlr.v4.runtime.misc.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +15,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
 import java.util.List;
+
 
 @Controller
 public class TaskController {
+
+    private final Logger logger = LoggerFactory.getLogger("TaskController");
 
     public String verificationCodeStr = "0";
     @Autowired
@@ -33,6 +41,7 @@ public class TaskController {
     public String addTask(@ModelAttribute Task task) throws NoSuchAlgorithmException {
         taskService.save(task);
         verificationCodeStr = task.getAccessId();
+        logger.info("["+ LocalDate.now() + "]" + "[INFO] Task ID: " + task.getId().toString() + " with access ID: " + task.getAccessId() + " /add > [AddTask Controller] [SUCCESS]");
         return "redirect:/verification-code";
     }
 
@@ -55,12 +64,24 @@ public class TaskController {
         if (verificationCode!=null)
         {
             List<Task> taskList = taskService.getByCode(verificationCode);
-            model.addAttribute("taskList", taskList);
-            model.addAttribute("taskSize", taskList.size());
+            if (taskList.size()==0){
+                logger.error("["+ LocalDate.now() + "]" + "[ERROR] Try to get access with incorrect verification code "
+                        + verificationCode
+                        + " /checkCode > [CheckCode Controller] [ERROR]");
+            }
+            else{
+                logger.info("[" + LocalDate.now() + "]" + "[INFO] Task with verification code: "
+                        + verificationCode + " accessed "
+                        + " /checkCode > [CheckCode Controller] [SUCCESS]");
+            }
+                model.addAttribute("taskList", taskList);
+                model.addAttribute("taskSize", taskList.size());
         }
         else{
             model.addAttribute("taskSize", 0);
             model.addAttribute("taskList", null);
+            logger.error("["+ LocalDate.now() + "]" + "[ERROR] Try to get access with no verification code "
+                    + " /checkCode > [CheckCode Controller] [ERROR]");
         }
         return "checkCode";
     }
